547b2017f6ac38ed1393a1d018cf1c40086c94d7
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2023-12-22 18:13:54 -0800

    added input search-box

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -10,9 +10,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -28,8 +30,27 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+
+            const searchString = event.target.value.toLocaleLowerCase();
+
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
