6f0c11d5aa74032fffc06970f207d9012dabb9ae
Mrunank Pawar <65391854+mrunankpawar@users.noreply.github.com>
2023-12-22 20:54:21 -0800

    storing theb original data

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
     console.log("constructor");
   }
@@ -31,6 +32,11 @@ class App extends Component {
 
   render() {
     console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input
@@ -38,20 +44,13 @@ class App extends Component {
           type="search"
           placeholder="Search Monsters"
           onChange={(event) => {
-            console.log(event.target.value);
-
-            const searchString = event.target.value.toLocaleLowerCase();
-
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
-
+            const searchField = event.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return { monsters: filteredMonsters };
+              return { searchField };
             });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
